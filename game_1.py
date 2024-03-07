2bc8d6766840d57dd6735ef670f8c076d5713876
MrPasto <kostyakas24@gmail.com>
2024-03-07 16:02:43 +0300

    pep8

---
+++ game_1.py
@@ -0,0 +1,36 @@
+from pygame import *
+from config import *
+from classes import Label, Rectangle
+
+# ФУНКЦИЯ ЗАПУСКАЕТ ИГРУ 1
+
+def game_1():
+    init()
+
+    screen = display.set_mode(SCREEN_SIZE)
+    display.set_caption('Nice game')
+    clock = time.Clock()
+
+    # ЗАГОЛОВОК
+    label_game = Label(text='Развитие логического мышления', font_size=64, font='Times New Roman')
+    label_rect = Rectangle(WIDTH // 2 - 475, 25, 950, 120)
+
+    # ФОН
+    start_screen_image = transform.scale(image.load('assets/start_screen.jpg'), (WIDTH, HEIGHT))
+
+    run = True
+    while run:
+        screen.blit(start_screen_image, (0, 0))
+        clock.tick(FPS)
+
+        for ev in event.get():
+            # ВЫКЛЮЧЕНИЕ
+            if ev.type == QUIT or ev.type == KEYDOWN and ev.key == K_ESCAPE:
+                run = False
+            # ВЕРНУТЬ КООРДИНАТЫ НАЖАТИЯ МЫШКОЙ
+            if ev.type == MOUSEBUTTONDOWN and ev.button == 1:
+                x, y = ev.pos
+
+        display.update()
+
+game_1()
