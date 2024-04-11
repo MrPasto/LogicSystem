9699ee1fe41a23ccf621fdadf9ccf61b53d07f31
MrPasto <kostyakas24@gmail.com>
2024-04-11 22:39:53 +0300

    Add rules window
    окно с кнопками игр. надо картинки сделать!

--- core/start_screen.py
+++ core/start_screen.py
@@ -21,14 +21,17 @@ def start_screen():
                           font_size=40, font_='Arial Narrow', italiano=True)
 
     # КНОПКИ ВЫБРАТЬ ИГРУ и ПРОЙТИ ТЕСТИРОВАНИЕ
-    x1, y1, sx1, sy1 = WIDTH // 2 - 350, 180, 700, 200
-    x2, y2, sx2, sy2 = WIDTH // 2 - 350, 430, 700, 200
+    x1, y1, sx1, sy1 = WIDTH // 2 - 350, 160, 700, 160
+    x2, y2, sx2, sy2 = WIDTH // 2 - 350, 340, 700, 160
+    x3, y3, sx3, sy3 = WIDTH // 2 - 350, 520, 700, 160
     button1 = Rectangle(x=x1, y=y1, x_size=sx1, y_size=sy1)
     button2 = Rectangle(x=x2, y=y2, x_size=sx2, y_size=sy2)
+    button3 = Rectangle(x=x3, y=y3, x_size=sx3, y_size=sy3)
     button_exit = Rectangle(x=20, y=(HEIGHT - 70), x_size=200, y_size=60)
 
     bt1_label = Label(text='Выбрать игру', font_size=85, font_='Arial Narrow')
     bt2_label = Label(text='Пройти тестирование', font_size=85, font_='Arial Narrow')
+    bt3_label = Label(text='Правила игры', font_size=85, font_='Arial Narrow')
     label_exit = Label(text='Выйти', font_size=40, font_='Arial Narrow')
     x, y, x_pos, y_pos = 0, 0, 0, 0
 
@@ -44,8 +47,8 @@ def start_screen():
 
         for ev in event.get():
             # ВЫКЛЮЧЕНИЕ
-            if ev.type == QUIT or ev.type == KEYDOWN and ev.key == K_ESCAPE:
-                run = False
+            if ev.type == QUIT:
+                return [None]
             # ВЕРНУТЬ КООРДИНАТЫ НАЖАТИЯ МЫШКОЙ
             if ev.type == MOUSEBUTTONDOWN and ev.button == 1:
                 x, y = ev.pos
@@ -72,6 +75,11 @@ def start_screen():
         else:
             button2.draw(screen, ORANGE, color_border=local_color)
 
+        if button3.collidepoint(x_pos, y_pos):
+            button3.draw(screen, CHEATS_ON_COLOR, color_border=local_color)
+        else:
+            button3.draw(screen, LIGHT_GRAY, color_border=local_color)
+
         if button_exit.collidepoint(x_pos, y_pos):
             button_exit.draw(screen, DARK_RED, color_border=GRAY)
             label_exit.draw_text(screen, color_=LIGHT_GRAY, position=(73, HEIGHT - 55))
@@ -82,8 +90,9 @@ def start_screen():
         label_rect.draw(screen, WHITE, color_border=local_color)
         label_game.draw_text(screen, color_=BLACK, position=(0, 85), cntr_x=True)
         label_authors.draw_text(screen, color_=BLACK, position=(WIDTH - 650, HEIGHT - 50))
-        bt1_label.draw_text(screen, color_=BLACK, position=(0, 275), cntr_x=True)
-        bt2_label.draw_text(screen, color_=BLACK, position=(0, 535), cntr_x=True)
+        bt1_label.draw_text(screen, color_=BLACK, position=(0, 240), cntr_x=True)
+        bt2_label.draw_text(screen, color_=BLACK, position=(0, 420), cntr_x=True)
+        bt3_label.draw_text(screen, color_=BLACK, position=(0, 600), cntr_x=True)
 
         # НАЖАТИЯ ПО КНОПКАМ
         if button1.collidepoint(x, y):
@@ -92,8 +101,11 @@ def start_screen():
         if button2.collidepoint(x, y):
             return 1, cheats
 
+        if button3.collidepoint(x, y):
+            return 2, cheats
+
         if button_exit.collidepoint(x, y):
-            return None
+            return [None]
 
         display.update()
 
